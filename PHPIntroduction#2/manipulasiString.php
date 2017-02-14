747c2251108705aab034b73afe0b8d46c8908d62
Fakhri Fauzan <fazan697@gmail.com>
2017-02-14 19:32:38 +0700

    Add files via upload

---
+++ PHPIntroduction#2/manipulasiString.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Manipulasi String</title>
+</head>
+<body>
+	1. Menghitung jumlah karakter </br>
+	2. Kapital semua </br>
+	3. Huruf kecil semua <br>
+	4. Membalikan string <br>
+
+	<form method="post">
+		<textarea name="string" rows="4" cols="40"></textarea><br>
+		<select name="pilihan">
+			<option value="pilih" selected="selected">-- Pilih --</option><br>
+			<option value="1">1</option><br>
+			<option value="2">2</option><br>
+			<option value="3">3</option><br>
+			<option value="4">4</option><br>
+		</select>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+
+	<?php  
+		
+	?>
+</body>
+</html>
